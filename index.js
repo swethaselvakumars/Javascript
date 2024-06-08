b4c6874695d1ebe8f2617ebdf4932e6a15e5ef94
swethaselvakumars <127839034+swethaselvakumars@users.noreply.github.com>
2024-06-08 11:40:06 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+document.getElementById("name").innerHTML="sweet"
+document.getElementById("age").innerHTML=20
+document.getElementById("Email").innerHTML="swe.gmail.com"
+document.getElementById("passedoutyear").innerHTML=2024
+document.getElementById("mobileno").innerHTML=38492101
+document.getElementById("region").innerText="<h1>INDIA</h1>"
