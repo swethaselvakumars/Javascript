ba3ea35d3c2d855a060fc49afe785c485f501c0e
Swetha Selvakumar <127839034+swethaselvakumars@users.noreply.github.com>
2024-06-25 21:36:45 +0530

    Add files via upload

---
+++ removeagivenstring.js
@@ -0,0 +1,16 @@
+var a="tamillnadu"
+var emp=""
+for(var i=0; i<=a.length-1; i++){
+    if(a[i]=='l'){
+       emp=emp+""
+        
+    }else{
+        emp=+a[i]
+    }
+    
+} 
+console.log(emp)
+
+// var z="tamillnadu"
+// console.log(z.replace("l",""))
+
